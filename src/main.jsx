99fca9a4290a3b7c1b50857d4bf6c95fd93cab67
Peggy Chang <prggy@mac.lan>
2025-06-06 03:17:00 +0800

    feat: add firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,25 @@
 // main.jsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-   <Provider store={store}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </Provider>
-);
+function MainApp() {
+  useEffect(() => {
+    AOS.init({ once: true, duration: 700, offset: 80 });
+  }, []);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<MainApp />);
